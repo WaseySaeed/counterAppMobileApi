e3440ebd9a92a307bb885686d5f4149a76af816d
Wasey Saeed <94783648+WaseySaeed@users.noreply.github.com>
2025-02-04 22:20:19 +0400

    Update CounterAppTest.java

--- src/main/java/com/exinity/mobile/test/CounterAppTest.java
+++ src/main/java/com/exinity/mobile/test/CounterAppTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.*;
 public class CounterAppTest extends BaseClassMobile {
 
 
-    @Test
+    @Test(description = "Validating the initial state of app, the increment counter by 1 and reset button functionality")
     public void validate_Counter_App_Initial_State_And_Counter_Increment_And_Reset() {
         CounterAppTestPage counterAppTestPage = new CounterAppTestPage(driver);
         MobileAssertion.validateUiElements(counterAppTestPage);
@@ -17,14 +17,14 @@ public class CounterAppTest extends BaseClassMobile {
         MobileAssertion.validateResetBtnFunctionality(counterAppTestPage.validatingResetBtnFunctionality());
     }
 
-    @Test
+    @Test(description = "Validation the selection of date and pressing cancel button on date picker")
     public void validate_No_Date_Selected_And_Date_Selected() {
         CounterAppTestPage counterAppTestPage = new CounterAppTestPage(driver);
         counterAppTestPage.validateCalendarFunctionality("cancelBtn");
         counterAppTestPage.validateCalendarFunctionality("selectDate");
     }
 
-    @Test
+    @Test(description = "Verifying the failure of the test case as the selected date is not displayed")
     public void failedTestCaseOfDateNotBeingDisplayed()
     {
         CounterAppTestPage counterAppTestPage = new CounterAppTestPage(driver);
